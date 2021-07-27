d1cfac0a7b443740259cfb71a671ee9d94c79380
Arion Mathias <arion.mathias@gmail.com>
2021-07-27 09:55:41 -0300

    first commit

---
+++ src/main/java/com/grupoestudo/GerenciaGrupoEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.grupoestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaGrupoEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaGrupoEstudosApplication.class, args);
+	}
+
+}
