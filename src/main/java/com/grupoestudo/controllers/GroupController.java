08552162a633d368b5cef0cdfa566564b3a3c30e
Arion Mathias <arion.mathias@gmail.com>
2021-07-30 20:11:03 -0300

    change database to postgre

--- src/main/java/com/grupoestudo/controllers/GroupController.java
+++ src/main/java/com/grupoestudo/controllers/GroupController.java
@@ -1,9 +1,6 @@
 package com.grupoestudo.controllers;
 
-import java.util.Arrays;
 import java.util.List;
-
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.grupoestudo.models.GroupModel;
-import com.grupoestudo.models.MemberModel;
 import com.grupoestudo.service.GroupService;
 
 /**
